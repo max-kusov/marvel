ae0e8973c356fa630b2d42ece00875f8f54f3e37
Kusov <Nutsmax88@gmail.com>
2022-02-01 13:43:41 +0400

    тренировка на классовых компонентах, использование API

---
+++ src/components/charList/CharList.js
@@ -0,0 +1,98 @@
+import { Component } from 'react';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import MarvelService from '../../services/MarvelService';
+
+import './charList.scss';
+import abyss from '../../resources/img/abyss.jpg';
+
+
+class CharList extends Component {
+
+    state = {
+        charList: [],
+        loading: true,
+        error: false,
+        newItemLoading: false,
+        offset: 210
+    }
+
+    marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.onRequest()
+    }
+
+
+    onRequest = (offset) => {
+        this.onCharListLoading()
+        this.marvelService.getAllCharacters(offset)
+            .then(this.onCharListLoaded)
+            .catch(this.onError)
+    }
+
+    onCharListLoading = () => {
+        this.setState({
+            newItemLoading: true
+        })
+    }
+
+    onCharListLoaded = (newCharList) => {
+        this.setState(({ offset, charList }) => ({
+            charList: [...charList, ...newCharList],
+            loading: false,
+            newItemLoading: false,
+            offset: offset + 9
+        }))
+    }
+
+    onError = () => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    }
+
+    renderItems(arr) {
+        const items = arr.map(item => {
+            return (
+                <li className="char__item"
+                    key={item.id}
+                    onClick={() => this.props.onCharSelected(item.id)}>
+                    <img src={item.thumbnail} alt={item.name} />
+                    <div className="char__name">{item.name}</div>
+                </li >
+            )
+        })
+        return (
+            <ul className="char__grid">
+                {items}
+            </ul>
+        )
+    }
+    render() {
+        const { charList, loading, error, offset, newItemLoading } = this.state;
+
+        const items = this.renderItems(charList);
+
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const spinner = loading ? <Spinner /> : null;
+        const content = !(loading || error) ? items : null;
+
+        return (
+            <div className="char__list">
+                {errorMessage}
+                {spinner}
+                {content}
+                <button className="button button__main button__long">
+                    <div
+                        className="inner"
+                        disabled={newItemLoading}
+                        onClick={() => this.onRequest(offset)}>load more</div>
+                </button>
+            </div>
+        )
+    }
+}
+
+export default CharList;
